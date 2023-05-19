c559b7b8a0598a1de4e8756a4464a68ce074065e
jwoohaha <jwoohaha@gmail.com>
2023-05-19 14:18:07 +0900

    feat: implement MovieCard, MovieCardList - show Trending, top 20 rated movies

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,55 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
 
 Vue.use(Vuex)
+const API_URL = 'http://127.0.0.1:8000'
+const TMDB_API_URL = 'https://api.themoviedb.org/3/trending/movie/week?'
 
 export default new Vuex.Store({
   state: {
+    topRatedMovies: [],
+    trendingMovies: []
   },
   getters: {
   },
   mutations: {
+    GET_TOP_RATED_MOVIES(state, movies) {
+      state.topRatedMovies = movies
+    },
+    GET_TRENDING_MOVIES(state, trendingMovieList) {
+      state.trendingMovies = trendingMovieList
+    }
   },
   actions: {
+    getTopRatedMovies(context) {
+      axios({
+        method: 'get',
+        url: `${API_URL}/movies/rating_top20/`,
+      })
+        .then((res) => {
+        // console.log(res, context)
+          context.commit('GET_TOP_RATED_MOVIES', res.data)
+        })
+        .catch((err) => {
+        console.log(err)
+      })
+    },
+    getTrendingMovies(context) {
+      axios({
+        method: 'get',
+        url: TMDB_API_URL,
+        params: {
+        'api_key': process.env.VUE_APP_TMDB_API_KEY
+        }
+      })
+      .then((response) => {
+        const trendingMovieList = response.data.results
+        context.commit('GET_TRENDING_MOVIES', trendingMovieList)
+      })
+      
+    },
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>MovieCardList</h1>
+    <br>
+    <h1>Trending</h1>
+    <MovieCardList :movies="trendingMovies"/>
+    <br>
+    <h1>Top Rated</h1>
+    <MovieCardList :movies="topRatedMovies"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MovieCardList from '@/components/MovieCardList.vue'
+import { mapState } from 'vuex'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    MovieCardList,
+  },
+  computed: {
+    ...mapState({
+      topRatedMovies:state => state.topRatedMovies,
+      trendingMovies:state => state.trendingMovies
+    })
+  },
+  created() {
+    this.getTopRatedMovies()
+    this.getTrendingMovies()
+  },
+  methods: {
+    getTopRatedMovies() {
+      this.$store.dispatch('getTopRatedMovies')
+    },
+    getTrendingMovies() {
+      this.$store.dispatch('getTrendingMovies')
+    }
   }
 }
 </script>
